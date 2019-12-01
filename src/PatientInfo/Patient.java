d10c31b8e42b68bb003f68a7337285ba6db14e36
ashp1731 <ashp1731@gmail.com>
2019-12-01 16:08:52 -0500

    Initial upload.

---
+++ src/PatientInfo/Patient.java
@@ -0,0 +1,9 @@
+package PatientInfo;
+import java.util.*;
+public class Patient
+{
+	private int setID, patientID;
+	private String name, race, primaryLanguage, ssn;
+	private char sex, maritalStatus, patientDeathInd;
+	private Date DOB;
+}
---
+++ src/PatientInfo/test.java
@@ -0,0 +1,45 @@
+package PatientInfo;
+
+import java.util.*;
+import java.io.*;
+
+public class test
+{
+	public static void main(String[]args)
+	{
+		File f = new File("messages.txt");
+		
+		try {
+			Scanner input = new Scanner(f);
+			String s = "";
+			ArrayList<String> pid = new ArrayList<>();
+			while(input.hasNext())
+			{
+				s = input.nextLine();
+				if(s.startsWith("PID"))
+				{
+					pid.add(s);
+				}
+			}
+			System.out.println(pid.size());
+			for(int i = 0; i < pid.size(); i++)
+			{
+				//System.out.println(pid.get(i));
+				String[] arrayPID = pid.get(i).split("\\|");
+//				getPatienInfo(arrayPID);
+				System.out.println(Arrays.toString(arrayPID));
+			}
+
+		}
+		catch(FileNotFoundException ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	
+
+
+
+	
+	
+}
