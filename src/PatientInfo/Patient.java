09fef87ff7b2f76e8125da0fc5854fa2f5434b19
ashp1731 <ashp1731@gmail.com>
2019-12-01 16:15:52 -0500

    removing every thing.

--- src/PatientInfo/Patient.java
+++
@@ -1,9 +0,0 @@
-package PatientInfo;
-import java.util.*;
-public class Patient
-{
-	private int setID, patientID;
-	private String name, race, primaryLanguage, ssn;
-	private char sex, maritalStatus, patientDeathInd;
-	private Date DOB;
-}
