baeac2abf67d8bae0577ecda75ce4142c6572a55
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2019-12-06 14:29:16 -0500

    hello

--- src/PatientInfo/test.java
+++ src/PatientInfo/test.java
@@ -9,27 +9,39 @@ public class test
 	// Test program to read file
 	public static void main(String[]args)
 	{
+		//outside the src paste file
 		File f = new File("messages.txt");
 		
 		try {
+			//open the file using sccanner object and read each line
 			Scanner input = new Scanner(f);
-			String s = "";
+			//initialize the string
+			String s ;
+			
 			ArrayList<String> pid = new ArrayList<>();
 			while(input.hasNext())
 			{
+				//1.we read line and put it in string
+				
 				s = input.nextLine();
+				//2.and checked the  string start with pid
 				if(s.startsWith("PID"))
 				{
+					//3.if its true its add to the array list
 					pid.add(s);
 				}
 			}
+			//size of pid arraylist
 			System.out.println(pid.size());
 //			System.out.println(pid);
+			//we are using for loop in array list
 			for(int i = 0; i < pid.size(); i++)
 			{
 				//System.out.println(pid.get(i));
 				String[] arrayPID = pid.get(i).split("\\|");
 				Patient patient = new Patient();
+				//we are getting the array from arraylist and passing to the method.(getPatientInfo(arrayPID)
+				//for  1 patient we have create one patient object from that method patient object is getting info of patient 1 from array
 				patient = getPatientInfo(arrayPID);
 				System.out.println(Arrays.toString(arrayPID));
 			}
