6a02be0b39c7bedf0077903ac741e43436d44679
ashp1731 <ashp1731@gmail.com>
2019-12-11 13:15:31 -0500

    formatting date

--- src/PatientInfo/StringToDate.java
+++ src/PatientInfo/StringToDate.java
@@ -6,7 +6,18 @@ import java.text.SimpleDateFormat;
 public class StringToDate {
 
 	public static Date convertStringToDate(String s, String format) throws Exception {
-		Date dateArray = new SimpleDateFormat(format).parse(s);
-		return dateArray;
+		Date date = new SimpleDateFormat(format).parse(s);
+		String strDate = new SimpleDateFormat("MM/DD/YYYY").format(date);
+		System.out.println(strDate);
+		return date = new SimpleDateFormat("MM/DD/YYYY").parse(strDate);
 	}
+
+//	public static void main(String[] args) {
+//		try {
+//			System.out.println(convertStringToDate("19860131", "yyyymmdd"));
+//		} catch (Exception ex) {
+//			System.out.println(ex);
+//		}
+//
+//	}
 }
