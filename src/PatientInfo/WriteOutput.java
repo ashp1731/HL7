d5f2f42860e984cbcd8b3cd50151988ff2db8cea
ashp1731 <ashp1731@gmail.com>
2019-12-10 21:45:19 -0500

    file write logic

--- src/PatientInfo/WriteOutput.java
+++ src/PatientInfo/WriteOutput.java
@@ -4,34 +4,27 @@ import java.io.FileNotFoundException;
 
 public class WriteOutput {
 
-	public static void writeToFile(Patient[] arrayPatient)  {
-try {
+	public static void writeToFile(Patient[] arrayPatient) {
+
 		String s;
-		java.io.File file =new java.io.File("patient-summary.txt");
-		try (
-		java.io.PrintWriter output=new java.io.PrintWriter(file);
-		){
-		if(file.exists()) {
-			System.out.println("file already exits");
-			System.exit(1);
+		java.io.File file = new java.io.File("patient-summary.txt");
+		try {
+			java.io.PrintWriter output = new java.io.PrintWriter(file);
+			if (file.exists()) {
+				System.out.println("file already exits");
+				System.exit(0);
+			}
+
+			for (int i = 0; i < arrayPatient.length; i++) {
+				s = arrayPatient.toString();
+				// Logic to write data into file
+
+				output.print(s);
+			}
 		}
-		
-		for (int i = 0; i < arrayPatient.length; i++) {
-			s = arrayPatient.toString();
-			// Logic to write data into file
-			
-			output.print(s);
-		}
-		}
-}
-		catch (Exception ex){
+		catch(Exception ex) {
 			System.out.println(ex);
 		}
+
 	}
 }
-
-
-	
-	
-	
-
