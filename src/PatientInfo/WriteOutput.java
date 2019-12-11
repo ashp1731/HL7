202d7eba44d27f091cc9f095c1b3986bc5c52b2a
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2019-12-10 21:04:37 -0500

    writeoutput

--- src/PatientInfo/WriteOutput.java
+++ src/PatientInfo/WriteOutput.java
@@ -9,6 +9,12 @@ public class WriteOutput {
 		for(int i = 0; i < arrayPatient.length; i++) {
 			s = arrayPatient.toString();
 			// Logic to write data into file
+			java.io.File file =new java.io.File("messages.txt");
+			if(file.exists()) {
+				System.out.println("file already exits");
+				system.ex
+			}
+			java.io.PrintWriter output=new java.io.PrintWriter output(file);
 		}
 	}
 }
